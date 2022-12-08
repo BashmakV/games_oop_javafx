8565321fdcbd91131bdfd4ee587511153617e00c
BashmakV <dolka-lenka@mail.ru>
2022-12-08 13:00:58 +0300

    Добавил 2 метода.

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -4,7 +4,7 @@ public class Win {
     public static boolean horizontal(int[][] board, int row) {
         boolean result = true;
         for (int i = 0; i < board[row].length; i++) {
-            if (board[row][i] != '1') {
+            if (board[row][i] != 1) {
                 result = false;
                 break;
             }
@@ -15,7 +15,7 @@ public class Win {
     public static boolean vertical(int[][] board, int column) {
         boolean result = true;
         for (int i = 0; i < board.length; i++) {
-            if (board[i][column] != '1') {
+            if (board[i][column] != 1) {
                 result = false;
                 break;
             }
@@ -24,13 +24,13 @@ public class Win {
     }
 
     public static boolean check(int[][] board) {
-        boolean res = false;
+        boolean result = false;
         for (int i = 0; i < board.length; i++) {
-            if (board[i][i] == '1' && (horizontal(board, i) || vertical(board, i))) {
-                res = true;
+            if (board[i][i] == 1 && (horizontal(board, i) || vertical(board, i))) {
+                result = true;
                 break;
             }
         }
-        return res;
+        return result;
     }
 }
