4e083935dd24d5dc0180c711502569148d9f9ac3
BashmakV <dolka-lenka@mail.ru>
2022-12-08 12:40:06 +0300

    Добавил 2 метода.

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -11,6 +11,7 @@ public class Win {
         }
         return result;
     }
+
     public static boolean vertical(int[][] board, int column) {
         boolean result = true;
         for (int i = 0; i < board.length; i++) {
@@ -21,6 +22,7 @@ public class Win {
         }
         return result;
     }
+
     public static boolean check(int[][] board) {
         boolean result = false;
         for (int i = 0; i < board.length; i++) {
